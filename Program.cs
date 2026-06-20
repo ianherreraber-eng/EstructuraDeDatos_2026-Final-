fdd7293c5dced1470cc67f1c4de4be3ad1befdfd
ianherreraber-eng <ianherreraber@gmail.com>
2026-06-20 00:35:16 -0600

    Resolviendo conflicto con el codigo correcto de la Clase 9

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Diagnostics; // Librería obligatoria para usar Stopwatch
+using System.Diagnostics; 
 
 class Clase9_Memoization
 {
@@ -7,7 +7,7 @@ class Clase9_Memoization
     {
         Console.WriteLine("=== Benchmark: Fuerza Bruta vs Memoization ===\n");
         
-        int n = 40; // Prueba con 40. Si pones 50 en Fuerza Bruta, tu PC podría congelarse.
+        int n = 40; 
         Stopwatch sw = new Stopwatch();
 
         // --- MÉTODO 1: FUERZA BRUTA ---
@@ -21,7 +21,6 @@ class Clase9_Memoization
         Console.ResetColor();
 
         // --- MÉTODO 2: MEMOIZATION (Caché) ---
-        // Inicializamos el arreglo de caché con puros -1 (que significa "vacío")
         long[] cache = new long[n + 1];
         for (int i = 0; i <= n; i++) 
         {
@@ -38,7 +37,6 @@ class Clase9_Memoization
         Console.ResetColor();
     }
 
-    // 1. Fuerza Bruta (Ineficiente, calcula lo mismo una y otra vez)
     public static long FibonacciInseguro(int n)
     {
         if (n == 0) return 0;
@@ -46,16 +44,13 @@ class Clase9_Memoization
         return FibonacciInseguro(n - 1) + FibonacciInseguro(n - 2);
     }
 
-    // 2. Memoization (Inteligente, guarda en caché)
     public static long FibonacciPro(int n, long[] cache)
     {
         if (n == 0) return 0;
         if (n == 1) return 1;
         
-        // ¡LA MAGIA!: Si el cálculo ya existe en el caché, devuélvelo inmediatamente
         if (cache[n] != -1) return cache[n];
         
-        // Si no existe, calcúlalo y GUÁRDALO en el caché para el futuro
         cache[n] = FibonacciPro(n - 1, cache) + FibonacciPro(n - 2, cache);
         return cache[n];
     }
